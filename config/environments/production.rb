635d3f5934316af20e2002829056a66a37b924f0
vincent viricel <vincent.viricel@gmail.com>
2017-03-22 17:39:06 +0100

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'http://hacksury.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
+    :authentication => 'login',
+    :domain => 'hacksury.herokuapp.com', # your domain to identify your server when connecting
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
