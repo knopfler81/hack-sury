a671a703f7fbee3de593604b08bfaab1c9e23ebb
vincent viricel <vincent.viricel@gmail.com>
2017-03-22 17:20:14 +0100

    attempt to fix heroku mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -58,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "easy_trip_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'hacksury.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://hacksury.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
